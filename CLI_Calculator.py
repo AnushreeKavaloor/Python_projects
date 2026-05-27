ef5e06543bf141b0120a2c59ddd9f51bc87ff3eb
AnushreeKavaloor <anushreekavaloor@gmail.com>
2026-05-27 23:22:50 +0530

    fix: exit before number input + add choice.lower()
    fix: correct exit flow and syntax error in calculator
    
    - Fixed SyntaxError: added missing quote in 'X' check
    - Moved 'x' exit condition before num1/num2 input prompts
    - Added choice.lower() to handle both 'x' and 'X' cases

--- CLI_Calculator.py
+++ CLI_Calculator.py
@@ -1,10 +1,13 @@
 while True:
 	try:
-		num1 = float(input("Enter first number: "))
-		num2 = float(input("Enter second number: "))
 		print("–––MENU–––")
 		print("+\n-\n*\n/\n%\n**\nx or X for EXIT")
 		choice = input("Enter your choice: ")
+		if choice.lower()=='x':
+			print("Exiting....")
+			break
+		num1 = float(input("Enter first number: "))
+		num2 = float(input("Enter second number: "))
 		match choice:
 			case '+':
 				result = num1 + num2
@@ -20,22 +23,20 @@ while True:
 					result = num1 / num2
 					print(f"Result: {result}")
 				else:
-					print("Division by zero is impossible.")
+					print("Division by zero is impossible")
 			case '%':
 				if num2!=0:
 					result = num1 % num2
 					print(f"Result: {result}")
 				else:
-					print("Division by zero is invalid to find remainder.")
+					print("Division by zero is invalid to find remainder")
 			case '**':
 				result = num1**num2
 				print(f"Result: {result}")
-			case 'X' | 'x':
-					print("Exiting....")
-					break
+			
 			case _:
-					print("Invalid Operation!")
+					print("Invalid Operation")
 	except ValueError:
-		print("You must enter only numbers.")
+		print("You must enter only numbers")
 	finally:
 		print("Operation completed!")
